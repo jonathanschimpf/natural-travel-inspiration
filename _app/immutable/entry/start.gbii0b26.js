e2684f26478d0b948d0275ad3e2c652105c28dee
jonathanschimpf <jonathan@jonathanschimpf.com>
2024-02-10 19:54:56 +0000

    Deploying to gh-pages from @ jonathanschimpf/natural-travel-inspiration@0899a3c2ab9b3771bf21c56c78430353bfabc992 🚀

---
+++ _app/immutable/entry/start.gbii0b26.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.WPZruK6_.js";export{t as start};
---
+++ _app/immutable/nodes/3.6CFNCA7x.js
@@ -0,0 +1,18 @@
+import{s as i,n as t}from"../chunks/scheduler.J7dZX9xK.js";import{S as n,i as l,e as h,c as u,k as c,l as p,g as m,d}from"../chunks/index.UhQ6_4fn.js";import{b}from"../chunks/paths.HFiIr-9V.js";function v(r){let e,s=`<div class="image-and-self-credit-container svelte-1gqw796"><img src="${`${b}/assets/south_kaibab_day-hike.png`}" class="about-south_kaibab_image svelte-1gqw796" alt="South Kaibab Day Hike"/> <p class="giving-yourself-credit svelte-1gqw796">South Kaibab Trail @ Grand Canyon National Park. Arizona, USA. Photo by Jonathan Schimpf (me).</p></div> <p class="about-writeup svelte-1gqw796"><span class="natural-span svelte-1gqw796">Natural Travel Inspiration</span> — we all live in cities or suburbs
+		and we work from businesses, offices or work from home. Having manmade structures be the main
+		setting where many of us spend most of our time, we should get out and explore. Nature is
+		beautiful and life is short, we should enjoy it while we can. I&#39;ve lived and worked in and
+		around a city for a long time, but in the evenings or on weekends I have always tried to get out
+		and enjoy what was here before I was born. Many of my travels have revolved around it. So I made
+		this web project as a way to inspire myself and others. It pulls random images from the
+		<a class="unsplash-link svelte-1gqw796" href="https://www.unsplash.com" rel="noreferrer" target="_blank" aria-label="Unsplash Link">Unsplash<span class="copyright-span svelte-1gqw796">©</span></a>
+		API that were created by photographers from all over the world that contribute their images to that
+		platform. Each random image presented credits them below it if you&#39;re curious. If you would prefer
+		to be slightly more specific about a random image you can hit the form below the &#39;Random Natural
+		Inspiration&#39; button to search. No guarantees! This simple web project was the first time that I have
+		built anything using
+		<a class="svelte-link svelte-1gqw796" href="https://kit.svelte.dev/" rel="noreferrer" target="_blank" aria-label="SvelteKit">SvelteKit</a>
+		and I don&#39;t believe it will be the last. You can check out the GitHub repository
+		<a class="natural-travel-repository svelte-1gqw796" href="https://github.com/jonathanschimpf/natural-travel-inspiration" rel="noreferrer" target="_blank" aria-label="Natural Travel Inspiration Repository">here</a>.
+
+		<br/> <br/> - Jonathan Schimpf</p>`;return{c(){e=h("div"),e.innerHTML=s,this.h()},l(a){e=u(a,"DIV",{class:!0,"data-svelte-h":!0}),c(e)!=="svelte-1ezylwo"&&(e.innerHTML=s),this.h()},h(){p(e,"class","about-container svelte-1gqw796")},m(a,o){m(a,e,o)},p:t,i:t,o:t,d(a){a&&d(e)}}}class y extends n{constructor(e){super(),l(this,e,null,v,i,{})}}export{y as component};
