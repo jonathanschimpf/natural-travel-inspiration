ba422ca6842710766f9bdd0793eb24f77761738d
Jawnathan <jonathan@jonathanschimpf.com>
2024-02-13 17:44:47 -0500

    git comm it dash m

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-const production = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
-	base: production ? '/natural-travel-inspiration/' : '/',
 	plugins: [sveltekit()]
 });
