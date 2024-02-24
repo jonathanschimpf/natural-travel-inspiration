cc6c5b21dab6523c148cee7ee7ae7e1a41ff67a7
jonathanschimpf <jonathan@jonathanschimpf.com>
2024-02-24 22:16:29 +0000

    Deploying to gh-pages from @ jonathanschimpf/natural-travel-inspiration@c3e5c41cc9fa622c69017d5c503c441fba4ca431 🚀

---
+++ _app/immutable/entry/start.Bs3hYHrk.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Bn9klwqf.js";export{t as start};
---
+++ _app/immutable/nodes/3.7q1XS2WV.js
@@ -0,0 +1,20 @@
+import{s as o,n as t}from"../chunks/scheduler.BXxgt0ok.js";import{S as i,i as l,e as h,c as b,k as u,l as p,g as m,d as c}from"../chunks/index.CBX-e1eN.js";import{b as d}from"../chunks/paths.BS6mbjIE.js";function f(r){let e,s=`<div class="image-and-self-credit-container svelte-18nbl1b"><img src="${`${d}/assets/south_kaibab_day-hike.png`}" class="about-south_kaibab_image svelte-18nbl1b" alt="South Kaibab Day Hike"/> <p class="giving-yourself-credit svelte-18nbl1b">South Kaibab Trail @ Grand Canyon National Park. Arizona, USA. Photo by
+			<a class="link-to-my-dev-page-tiny svelte-18nbl1b" href="https://jonathanschimpf.dev" rel="noreferrer" target="_blank" aria-label="JonathanSchimpf.dev">Jonathan Schimpf (me)</a>.</p></div> <p class="about-writeup svelte-18nbl1b"><span class="natural-span svelte-18nbl1b">Natural Travel Inspiration</span> — We all live in cities or suburbs
+		and we work from businesses, offices or work from home. Having manmade structures be the main
+		setting where many of us spend most of our time, we should take the time to explore. Given the
+		beauty of nature and the shortness of our lifetimes, we should enjoy it while we can. I&#39;ve lived
+		and worked in and around a city for a long time, but in the evenings or on weekends I have
+		always sought out opportunities to get out and enjoy what was here before I was born. Many of my
+		travels have revolved around it. So I made this web project as a way to inspire myself and
+		others. It pulls random images from the
+		<a class="unsplash-link svelte-18nbl1b" href="https://www.unsplash.com" rel="noreferrer" target="_blank" aria-label="Unsplash Link">Unsplash<span class="copyright-span svelte-18nbl1b">©</span></a>
+		API that were created by photographers from all over the world that contribute their images to that
+		platform. Each random image presented credits them below it if you&#39;re curious. If you would prefer
+		to be slightly more specific about a random image you can hit the form below the &#39;Random Natural
+		Inspiration&#39; button to search. No guarantees! This simple web project was the first time that I have
+		built anything using
+		<a class="svelte-link svelte-18nbl1b" href="https://kit.svelte.dev/" rel="noreferrer" target="_blank" aria-label="SvelteKit">SvelteKit</a>
+		 (a powerful and flexible framework for building web applications) and I don&#39;t believe it will be the last. I really enjoy it.You can check out the GitHub repository
+		<a class="natural-travel-repository svelte-18nbl1b" href="https://github.com/jonathanschimpf/natural-travel-inspiration" rel="noreferrer" target="_blank" aria-label="Natural Travel Inspiration Repository">here</a>.
+
+		<br/> <br/> - Jonathan Schimpf</p>`;return{c(){e=h("div"),e.innerHTML=s,this.h()},l(a){e=b(a,"DIV",{class:!0,"data-svelte-h":!0}),u(e)!=="svelte-1mkvfir"&&(e.innerHTML=s),this.h()},h(){p(e,"class","about-container svelte-18nbl1b")},m(a,n){m(a,e,n)},p:t,i:t,o:t,d(a){a&&c(e)}}}class y extends i{constructor(e){super(),l(this,e,null,f,o,{})}}export{y as component};
