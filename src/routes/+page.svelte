e1578437a1677e32d86aaf9f2ba62286b5e23a6b
Jawnathan <jonathan@jonathanschimpf.com>
2024-02-28 18:39:13 -0500

    src/lib/Nav.svelte mishap + external blacklist .js file module

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,393 +3,15 @@
 	import PhotoCard from '../components/PhotoCard.svelte';
 	import { writable } from 'svelte/store';
 
+	// EXTERNAL BLACKLISTED ID'S STORED IN THE JS MODULE FILE BELOW
+	import { blacklist } from '../components/Blacklisted-IDs.js';
+
 	// PUBLIC ACCESS KEY FROM THE Unsplash COMPANY AND KEPT PRIVATE IN ENVIRONMENT VARIABLES
 	const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
 	// THIS STORE MAINTAINS A SET OF PHOTO IDs THAT HAVE BEEN SEEN BY THE IP
 	const seenPhotos = writable(new Set());
 
-	// Unsplash PHOTO IDs THAT ARE BLACKLISTED
-	// BECAUSE THEY ARE NOT A GREAT
-	// FIT FOR THIS PROJECT — WORK IN PROGRESS
-	const blacklist = new Set([
-		'UOkZT4JT8fw',
-		'parczv2B-ZM',
-		'xRr9Lz3xfYs',
-		'uYoVf9I6ANI',
-		'mmzuZhihp30',
-		'jqgsM3B9Fpo',
-		'tMzCrBkM99Y',
-		'IicyiaPYGGI',
-		'jAjvbh7vw9g',
-		'6c5ZTHnUUzA',
-		'OdBFUurPHjo',
-		'PSFfI7vbS3c',
-		'9JxubXPaidg',
-		'uNliRqlmBdg',
-		'jnJ89cuXMcs',
-		'F6Da4r2x5to',
-		'z8ct_Q3oCqM',
-		'RwHv7LgeC7s',
-		'2JwQoi-RBiI',
-		'3W9buupDI2k',
-		'10TSKDfW8X8',
-		'TkrRvwxjb_8',
-		'fsJB3KT2rj8',
-		'bBiuSdck8tU',
-		'h6zMgghzZr0',
-		'7AcMUSYRZpU',
-		'jin4W1HqgL4',
-		'El-egS_h_N4',
-		'MMJx78V7xS8',
-		'h5eLbyHzAcE',
-		'ITfHPUy0x9k',
-		's0vFLLZzOrs',
-		'v4e3JI7DDHI',
-		'vngzm4P2BTs',
-		'koy6FlCCy5s',
-		'sx6n_hgSmOc',
-		'9Z1KRIfpBTM',
-		'JP23z_-dA74',
-		'jt6QxZwSOCQ',
-		'8mCMQSq41gQ',
-		'Dqzsti6Fonc',
-		'KhsuK_wq-o4',
-		'rCFAY4WrbUY',
-		'4rDCa5hBlCs',
-		'hPxKmyCZ-Bs',
-		'KMYCSLP8uiU',
-		'jWv1ILisuSc',
-		'OOE4xAnBhKo',
-		'rleYWWVY4Pc',
-		'GVnUVP8cs1o',
-		'T8-kfC8W4b8',
-		'yjEt-n0qUl8',
-		'LNYdZutqsi0',
-		'd2aXuZD2kUc',
-		'OS0D-aJ54CY',
-		'6ArTTluciuA',
-		'6loxuv3aXkg',
-		'XxvXRmsH860',
-		'qPPWNeFVLFQ',
-		'pmhoQj3Ce1E',
-		'6-C0VRsagUw',
-		'8SXaMMWCTGc',
-		'h5wvMCdOV3w',
-		'CoIqQVwEGY0',
-		'I3jSS2YfmcU',
-		'0uO6qhd6Bi8',
-		'gofWd3XkKo0',
-		'NliTYm_jD5o',
-		'zPCj7SKoub8',
-		'gvb84QdErNs',
-		'-N_UwPdUs7E',
-		'choc7LYd98I',
-		'Tw_rTjWMuGo',
-		'WgT_B4mN0BA',
-		'yZNGVL_fWj8',
-		'pT8mKX61STI',
-		'NjT4O7WYmwk',
-		'wQImoykAwGs',
-		'cS6YoRIAK2w',
-		'_EMkxLdko9k',
-		'vzVGHfxZIZ8',
-		'F5zoQfoWtbM',
-		'U86FnrpRR0k',
-		'fG4PYBDmAkI',
-		'9Gs0qk9Z3WU',
-		'h3ki3bbJWk0',
-		'xjhgZCuvg3k',
-		'fMIsAL72P3U',
-		'5-jtsfuaLBw',
-		'AnKC9klnyKk',
-		'Ee0894MAyok',
-		'gyIRjKPXupE',
-		'jFCViYFYcus',
-		'eOpewngf68w',
-		'Rfflri94rs8',
-		'Lh2qwkppkf8',
-		'RdouF_LXaYI',
-		'049M_crau5k',
-		'IVbFRSEYGuA',
-		'sNVE_h_7naU',
-		'7KLa-xLbSXA',
-		'poXhq_-eANA',
-		'utoPXPPr5zc',
-		'jCL98LGaeoE',
-		'rFn64K0YQrI',
-		'vpkCTrnV2eE',
-		'JiphyElpixM',
-		'ciHYaPb8lUA',
-		'c1Jp-fo53U8',
-		'WK4lhYGRIzw',
-		'JWa5jZ1LkJY',
-		'MPKFmy_Xc_A',
-		'NEqEC7qa9FM',
-		'vUNQaTtZeOo',
-		'c4cWawGBFv8',
-		'rckx2mE9rOw',
-		'cCthPLHmrzI',
-		'dhw3LSuWyks',
-		'3U3Qegatt2o',
-		'QsWG0kjPQRY',
-		'42MaenKbPe0',
-		'9K9ipjhDdks',
-		'sFLVTqNzG2I',
-		'7ne3hNnojvU',
-		'j6dqEl13is4',
-		'hpTH5b6mo2s',
-		'19SC2oaVZW0',
-		'u4fWwlBhHJ8',
-		'ugnrXk1129g',
-		'Yl9YFBtZFcQ',
-		'SaWeuWtLQqE',
-		'NP_GHS5Bb-s',
-		'yu2Ay7LQmnY',
-		'_ReQ6GSqSaM',
-		'wxZBV8GiPlc',
-		'1OtUkD_8svc',
-		'PXXtq6bp6cs',
-		'qN8AMtHZ2nY',
-		'J2xHOsMNIn0',
-		'SDoMyyylpnc',
-		'SDoMyyylpnc',
-		'3Kv48NS4WUU',
-		'3Ltn2aWVNcM',
-		'vgazHbc0XWg',
-		'8UVZHt8AUoQ',
-		'Ed_DchxFfrU',
-		'uilVSfUGnWk',
-		'dSxAggoqPAk',
-		'F_moKY91WYc',
-		'2ZjJ7T6EeOg',
-		'mLXmJb3In8Q',
-		'T69h1_YfR-w',
-		'kIR55P1eXJI',
-		'mFbrMEVKCkc',
-		'UFnHt94r91w',
-		'pt7QzB4ZLWw',
-		'6xh7H5tWj9c',
-		'bnUXhIyf0i0',
-		'WJkc3xZjSXw',
-		'5S6_c_yCIwE',
-		'd4feocYfzAM',
-		'nTRDnDdDYk8',
-		'Rr3B0LH7W3k',
-		'7NBO76G5JsE',
-		'0D8_tYHIy0o',
-		'lsGWd4KgCD4',
-		'JiuVoQd-ZLk',
-		'qzgN45hseN0',
-		'ZeNABWS-k4Q',
-		'Dm-qxdynoEc',
-		'__0Yg6-lvP8',
-		'qd-dvtbS45Q',
-		'4ooRozrZdLM',
-		'wKjIeK4QSnk',
-		'4QWR5geRC9I',
-		'PjjMG6VyHs4',
-		'TMNyU2MFTGw',
-		'lIat5Z-N3JA',
-		'QMnYC8WVFM8',
-		'SBiVq9eWEtQ',
-		'uaO9cd5BQPc',
-		'sp-p7uuT0tw',
-		'sp-p7uuT0tw',
-		'Yh6K2eTr_FY',
-		'GzcI_rMNclY',
-		'CtxRK04XMKs',
-		'mppuJZUzhI0',
-		'AqXqL_V9qpM',
-		'6csuZQ9oZcI',
-		'TRhGEGdw-YY',
-		'NQSWvyVRIJk',
-		'FMWXjhIkmO0',
-		'VHUbeZMH6Yg',
-		'uJnUhGtI_Pk',
-		'hry9juw-x5k',
-		'50bzI1F6urA',
-		'J2jtcRbiL-4',
-		'zK049OFP4uI',
-		'enXOyujvTXQ',
-		'z_HDrTQxNEI',
-		'z_HDrTQxNEI',
-		'zXVk8mNl9M0',
-		'a0nbRUHimZw',
-		'a0nbRUHimZw',
-		'S9miGKjxmb4',
-		'uyNMxCFYMvo',
-		'gzMvo6cR6J0',
-		'b4l3Mn4pRKk',
-		'D4TooCIEyF4',
-		'2JuKK2ticlk',
-		'buF62ewDLcQ',
-		'Fz77XHwFYg0',
-		'WVEBqXRGcy8',
-		'xY_6ZENqcfo',
-		'TBl1-EKYFjs',
-		'KYTT8L5JLDs',
-		'XAmgFP_GvVA',
-		'5VDFTgpwV2g',
-		'whDrFMucHkc',
-		'G8rRItjrwkA',
-		'HkNKkRoiGk8',
-		'igX2deuD9lc',
-		'xg8z_KhSorQ',
-		'5G2G59V6IXg',
-		'LMq1gYdygxo',
-		'Qdi8UvGd1Ww',
-		'-BZc9Ee1qo0',
-		'RkBTPqPEGDo',
-		'RhjVGxILcqE',
-		'UnKqaCtnHwE',
-		'9YubwxAIpw0',
-		'Kvc5x3Bvfd4',
-		'jwTvCQQJXh0',
-		'0Kyp7uO-3RQ',
-		'3okHTqD9yUA',
-		'i9Q9bc-WgfE',
-		'a47qPRbHx7g',
-		'3LwMyv3FiUE',
-		'KRAazAqquaQ',
-		'OsMWWAL0saQ',
-		'a2zdgkDzFo8',
-		'9pvTSsNV2T4',
-		'hfLsVDvnKwY',
-		'-KdfxdUEVv0',
-		'XxDJ6bgq7Pw',
-		'P3DxOe-OJGA',
-		'-IZ2sgQKIhM',
-		'uzwTVzXqZcg',
-		'HgQOh4s_M-U',
-		'ap6baZcbAho',
-		'T7s_TnKO-dk',
-		'FO7bKvgETgQ',
-		'NWleDEFmYDg',
-		'gcOMSDzWvR8',
-		'Ke-ENe3ByiQ',
-		'4-KqFyHKRdo',
-		'oj8XzDDpK40',
-		'e6f8IaRQY7M',
-		'hnw3Al47-KE',
-		'ipH70OUZDrk',
-		'1awcLzy32Dg',
-		'FAn-NWH8Bzw',
-		'd6kSvT2xZQo',
-		'DuiPYwz3CBA',
-		'gd3t5Dtbwkw',
-		'JoH60FhTp50',
-		'SvdVrpLiocU',
-		'gMqBIvJfP0E',
-		'htNpsGOMQL8',
-		'2WBVLTKNo1k',
-		'hom1seYgTQQ',
-		'cfKC0UOZHJo',
-		'eGD69I3ODC4',
-		'02a4DSekRVg',
-		'G_1V58E0TyM',
-		'5P91SF0zNsI',
-		'NrN5Rvl89Lo',
-		'cOMpDHi3fsM',
-		'8jQFXXSTvbw',
-		'tSZZ2NBj1eE',
-		'-rSka4Bw-EU',
-		'TsMXK2W9hV0',
-		'J4aLKW3T0FE',
-		'ntA_kfsJQjw',
-		'REKXJ7JhwiI',
-		'SqBK8g4g_ug',
-		'zAQoZwidDbs',
-		'Jzery_MafWQ',
-		'H8vhhepiiaU',
-		'A0P3qTZcjJw',
-		'RaEFRWLy9ME',
-		'KT3WlrL_bsg',
-		'RtTxZBbJe6A',
-		'baTtO8UODDo',
-		'3MNzGlQM7qs',
-		'yrBbY1ljUYc',
-		'zqIOvV-D3JQ',
-		'IbLZjKcelpM',
-		'RsRpw1rvkJs',
-		'sTVYoJHXKv8',
-		'hji142JdgRw',
-		'iiLQN9P6Irc',
-		'ZRdSZeVKBaE',
-		'Ceuh97A6OYM',
-		'HG5QX-GXV5A',
-		'DD1fSz2HF1s',
-		'AHWrFYvRBuk',
-		'8Ogfqvw15Rg',
-		'8bsvwFWVcvE',
-		'VS1uBhwf8a4',
-		'tNDvFkxkBHo',
-		'ofuctQNpnk4',
-		'C7jBZEGciCU',
-		'6YmzwamGzCg',
-		'Sl1GrFYrQ1E',
-		'IfTKequW2Mk',
-		'ln5drpv_ImI',
-		'JjYhagSxnOU',
-		'KMn4VEeEPR8',
-		'jeIRRRu6dbE',
-		'L_uyuJzJAxc',
-		'qb14VmiCjBA',
-		'uOtPQtKI_A8',
-		'HDd-NQ_AMNQ',
-		'cbPh8sfpP70',
-		'50HS3AzJsvQ',
-		'iuqmGmst5Po',
-		'jp0Qlyv389g',
-		'1rQs3661lq4',
-		'lsoogGC_5dg',
-		'VyqvPLMSZrc',
-		'kGSapVfg8Kw',
-		'C6gxT-N56ns',
-		'eCqA-col90Q',
-		'4igCpD-Lnfg',
-		'1sim8ojvCbE',
-		'8H5kNkYmJYQ',
-		'4r_tHA3gsUY',
-		'Kl8S7XbWbzM',
-		'_3DI_vx2ygg',
-		'mBQIfKlvowM',
-		'b_wN4QemTzU',
-		'BXasVMRGsuo',
-		'odxB5oIG_iA',
-		'pBgnT4KH8d4',
-		's7fVceSaiiw',
-		'EwKXn5CapA4',
-		'BvijLrkWcHA',
-		'KHrwWI5rdo8',
-		'S_IV6KqGeso',
-		'PmPZkRQYTyI',
-		'zhqD0tw1peA',
-		'jUCQRQeRs3k',
-		'dGmUuyZo_c4',
-		'qWJcROZIeec',
-		'2tvkoVK8prc',
-		'pRRSpbCFUOM',
-		'TkBhnAUm74M',
-		'VV2_XVEjg5w',
-		'oWRVjFQIwAY',
-		'3nQ1BT_rC3U',
-		'm6FGop1tliQ',
-		'upldea-3F4w',
-		'bIybKPVJW8M',
-		'2cwEJy-0hfw',
-		'lX9NstRMiDU',
-		'741MfdrVr_I',
-		'cf2QWjRd4IE',
-		'y1qOT82fd8k',
-		'D9gWibH5_kY',
-		'Eek6SLvyNYU',
-		'lsDJyu_uJhw',
-		'uCqu7KVacG8',
-		'W-CuqaebiNY',
-		'PT4xBct05I0',
-		'rVbl6qgDPa8',
-		'jp0Qlyv389g'
-	]);
+	
 	// HOLDS THE CURRENT PHOTO OBJECT
 	let photo;
 	// STORES VARIOUS ERROR MESSSAGES
@@ -467,7 +89,7 @@
 	}
 
 	const MAX_ATTEMPTS = 20; // TRY 20 TIMES TO FIND A NON-BLACKLISTED ID (PHOTO)
-	const RETRY_DELAY = 1000; // WAIT1 SECOND BEFORE TRYING AGAIN
+	const RETRY_DELAY = 1000; // WAIT 1 SECOND BEFORE TRYING AGAIN
 
 	// THIS FUNCTION MAKES MULTIPLE ATTEMPTS TO FETCH
 	// A UNIQUE PHOTO THAT IS NOT ON THE BLACKLIST ABOVE
@@ -560,7 +182,7 @@
 <button class="random-natural-button" on:click={getUniquePhoto}>Random Natural Inspiration</button>
 
 <p class="click-below-message-longer">
-	<span class="smaller-down-arrow"> ⬇ </span>Search For A More Specific Random Image Below<br/>
+	<span class="smaller-down-arrow"> ⬇ </span>Search For A More Specific Random Image Below<br />
 	Single Word Searches Work Best With This API — You Could Always "Google" It.
 </p>
 
@@ -583,14 +205,7 @@
 	{/if}
 </div>
 
-
-
-
 <style>
-	/* .placeholder {
-		display: block;
-		z-index: 1;
-	} */
 	.image-container-on-page {
 		position: relative;
 	}
@@ -624,7 +239,7 @@
 	}
 
 	.click-below-message-longer {
-		margin-top: 25px;
+		margin-top: 20px;
 		margin-bottom: 0;
 		padding: 0;
 	}
@@ -691,6 +306,7 @@
 		margin-right: 0.5rem;
 		background-color: #535353;
 		font-size: 14px;
+		color: white;
 	}
 
 	.pico-form button {
@@ -718,20 +334,21 @@
 		border: 2px solid #3a3a3a !important;
 		outline: 2px solid #3a3a3a !important;
 		-webkit-box-shadow: 0 0 0 90px rgb(84, 84, 84) inset !important;
-		font-weight: normal !important;
+		-webkit-text-fill-color: white !important;
+		font-family: inherit !important;
 	}
 
 	input:-webkit-autofill {
 		border: 2px solid #3a3a3a !important;
 		outline: 2px solid #3a3a3a !important;
 		transition: background-color 5000s ease-in-out 0s;
-		font-weight: normal !important;
 	}
 
+	/*  Mobile Media Query's */
 
 	/* RESET HOVER STYLES FOR
 	 MOBILE DEVICES BECAUSE REASONS */
-	 @media (hover: none) {
+	@media (hover: none) {
 		button:hover {
 			background-color: #3a3a3a;
 			color: #fff;
@@ -745,17 +362,17 @@
 		}
 	}
 
-	/*  Mobile Media Query's */
-
 	@media (max-width: 525px) {
+		.click-below-message-longer,
 		.smaller-down-arrow {
-			font-size: 8px;
+			font-size: 11px;
 		}
 	}
 
-	@media (max-width: 525px) {
-		.click-below-message-longer {
-			font-size: 11px;
+	@media (max-width: 440px) {
+		.click-below-message-longer,
+		.smaller-down-arrow {
+			font-size: 8px;
 		}
 	}
 
@@ -771,7 +388,7 @@
 		}
 	}
 
-	@media (max-width: 425px) {
+	@media (max-width: 440px) {
 		.pico-form {
 			max-width: 300px !important;
 		}
