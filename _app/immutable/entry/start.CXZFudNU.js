39152f88dda4f397f2618a8054d97ded59d151e5
jonathanschimpf <jonathan@jonathanschimpf.com>
2024-03-07 20:39:37 +0000

    Deploying to gh-pages from @ jonathanschimpf/natural-travel-inspiration@8b88798570bb153d0fbbb9fa4bede498eff81055 🚀

---
+++ _app/immutable/entry/start.CXZFudNU.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.B708oxc1.js";export{t as start};
---
+++ _app/immutable/nodes/3.K-bC7rZ8.js
@@ -0,0 +1,19 @@
+import{s as M,a as P,n as q,b}from"../chunks/scheduler.xP9QEsb2.js";import{S as C,i as L,e as u,s as I,c as h,a as S,f as j,k as T,d as y,l as n,g as N,h as o,m as g}from"../chunks/index.B4ohFuCl.js";import{b as D}from"../chunks/paths.481z9sIs.js";import{f as w}from"../chunks/index.BQMALu5r.js";function G(H){let e,a,s,_,p,d,i,k=`South Kaibab Trail @ Grand Canyon National Park. Arizona, USA. Photo by
+			<a class="link-to-my-dev-page-tiny svelte-x4q44u" href="https://jonathanschimpf.dev" rel="noreferrer" target="_blank" aria-label="JonathanSchimpf.dev">Jonathan Schimpf (me)</a>.`,f,r,x=`<span class="natural-span svelte-x4q44u">Natural Travel Inspiration</span> — We all live in cities or suburbs
+		and we work from businesses, offices or work from home. Having manmade structures be the main
+		setting where many of us spend most of our time, exploring natural features is good for us all. Given the
+		beauty of nature and the shortness of our lifetimes, we should enjoy it while we can. I&#39;ve lived
+		and worked in + around a city for a long time, but in the evenings or on weekends I have
+		always sought out opportunities to get out and enjoy what was here before I was born. Many of my
+		travels have revolved around it. So I made this web project as a way to inspire myself and
+		others. It pulls random images from the
+		<a class="unsplash-link svelte-x4q44u" href="https://www.unsplash.com" rel="noreferrer" target="_blank" aria-label="Unsplash Link">Unsplash<span class="copyright-span svelte-x4q44u">©</span></a>
+		API that were created by photographers from all over the world that contribute their images to that
+		platform. Each random image presented credits them below it if you&#39;re curious. This simple web project was the first time that I have
+		built anything using
+		<a class="svelte-link svelte-x4q44u" href="https://kit.svelte.dev/" rel="noreferrer" target="_blank" aria-label="SvelteKit">SvelteKit</a>
+		 (a powerful and flexible framework for building web applications) and I don&#39;t believe it will be the last. I really enjoy it. You can check out the GitHub repository
+		<a class="natural-travel-repository svelte-x4q44u" href="https://github.com/jonathanschimpf/natural-travel-inspiration" rel="noreferrer" target="_blank" aria-label="Natural Travel Inspiration Repository">here</a>  —  continuous deployment via 
+		<a class="continuous-deployment-via-netlify svelte-x4q44u" href="https://www.netlify.com/" rel="noreferrer" target="_blank" aria-label="Continuous Deployment via Netlifyy">Netlify</a>.
+
+		<br/> <br/> - Jonathan Schimpf`,m,v;return{c(){e=u("div"),a=u("div"),s=u("img"),d=I(),i=u("p"),i.innerHTML=k,f=I(),r=u("p"),r.innerHTML=x,this.h()},l(t){e=h(t,"DIV",{class:!0});var l=S(e);a=h(l,"DIV",{class:!0});var c=S(a);s=h(c,"IMG",{src:!0,class:!0,alt:!0}),d=j(c),i=h(c,"P",{class:!0,"data-svelte-h":!0}),T(i)!=="svelte-3jopso"&&(i.innerHTML=k),c.forEach(y),f=j(l),r=h(l,"P",{class:!0,"data-svelte-h":!0}),T(r)!=="svelte-4w9g95"&&(r.innerHTML=x),l.forEach(y),this.h()},h(){P(s.src,_=`${D}/assets/south_kaibab_day-hike.png`)||n(s,"src",_),n(s,"class","about-south_kaibab_image svelte-x4q44u"),n(s,"alt","South Kaibab Day Hike"),n(i,"class","giving-yourself-credit svelte-x4q44u"),n(a,"class","image-and-self-credit-container svelte-x4q44u"),n(r,"class","about-writeup svelte-x4q44u"),n(e,"class","about-container svelte-x4q44u")},m(t,l){N(t,e,l),o(e,a),o(a,s),o(a,d),o(a,i),o(e,f),o(e,r)},p:q,i(t){t&&(p||b(()=>{p=g(s,w,{delay:100,duration:500}),p.start()})),t&&(m||b(()=>{m=g(r,w,{delay:100,duration:500}),m.start()})),t&&(v||b(()=>{v=g(e,w,{delay:100,duration:500}),v.start()}))},o:q,d(t){t&&y(e)}}}class U extends C{constructor(e){super(),L(this,e,null,G,M,{})}}export{U as component};
