9e3bb9ada65044b965875e87cb298ffcd482c21e
jonathanschimpf <jonathan@jonathanschimpf.com>
2024-03-09 15:14:21 +0000

    Deploying to gh-pages from @ jonathanschimpf/natural-travel-inspiration@3a0308384251ddf984c2849f03ae79b71f95bb95 🚀

---
+++ _app/immutable/chunks/paths.CSMYqapk.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_7o9qhm)==null?void 0:s.base)??"/natural-travel-inspiration";var a;const e=((a=globalThis.__sveltekit_7o9qhm)==null?void 0:a.assets)??t;export{e as a,t as b};
